b7b797f063cfea537ec9326689356e0f441e2734
jprinz <jprinz@conestogac.on.ca>
2017-05-23 11:47:07 -0400

    Coded to pg 287

---
+++ app/src/main/java/com/example/jprinz/notetoself/Note.java
@@ -0,0 +1,54 @@
+package com.example.jprinz.notetoself;
+
+/**
+ * Created by jprinz on 5/23/2017.
+ */
+
+public class Note {
+
+    private String mTitle;
+    private String mDescription;
+    private boolean mIdea;
+    private boolean mTodo;
+    private boolean mImportant;
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String mDescription) {
+        this.mDescription = mDescription;
+    }
+
+    public boolean isIdea() {
+        return mIdea;
+    }
+
+    public void setIdea(boolean mIdea) {
+        this.mIdea = mIdea;
+    }
+
+    public boolean isTodo() {
+        return mTodo;
+    }
+
+    public void setTodo(boolean mTodo) {
+        this.mTodo = mTodo;
+    }
+
+    public boolean isImportant() {
+        return mImportant;
+    }
+
+    public void setImportant(boolean mImportant) {
+        this.mImportant = mImportant;
+    }
+}
