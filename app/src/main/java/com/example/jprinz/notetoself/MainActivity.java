d595ea37802c5acad146b276aefe96d72ccb1263
JP <jprinz@conestogac.on.ca>
2017-05-23 17:20:33 -0400

    Code complete to end chapter 12; Still have some UI issues to work out

--- app/src/main/java/com/example/jprinz/notetoself/MainActivity.java
+++ app/src/main/java/com/example/jprinz/notetoself/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,14 +22,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        // Temporary code
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                // Create a new DialogShowNote called dialog
+                DialogShowNote dialog = new DialogShowNote();
+
+                // Send the note via the sendNoteSelected method
+                dialog.sendNoteSelected(mTempNote);
+
+                // Create the dialog
+                dialog.show(getFragmentManager(), "123");
             }
         });
+
+
     }
 
     @Override
@@ -47,6 +58,13 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == R.id.action_add) {
+
+            DialogNewNote dialog = new DialogNewNote();
+            dialog.show(getFragmentManager(), "");
             return true;
         }
 
